c756e945ad85b10e97955a7dac333bb4365937e1
barissonmez <private@barissonmez.net>
2016-06-19 00:48:36 +0300

    app.js file has been modified.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import $ from 'jquery';
-// import {Car} from './classes/car.js';
-// import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js';
 import {FleetDataService} from './services/fleet-data-service.js';
 import {ApplicationBase} from './framework/application-base.js';
@@ -25,63 +23,3 @@ export class App extends ApplicationBase {
 
 export let application = new App();
 application.show($('body'));
-
-// import {Button} from './ui/button.js';
-// import {Image} from './ui/image.js';
-// import {TitleBar} from './ui/title-bar.js';
-// import {DataTable} from './ui/data-table.js';
-// import {GoogleMap} from './ui/google-map.js';
-
-// let dataService = new FleetDataService();
-// dataService.loadData(fleet);
-//
-// let centerOfMap = {lat: 40.783661, lng: -73.965883};
-// let map = new GoogleMap(centerOfMap, dataService.cars);
-// map.appendToElement($('body'));
-
-
-// let headers = "License Make Model Miles".split(' ');
-// let dataService = new FleetDataService();
-// dataService.loadData(fleet);
-// let dt = new DataTable(headers, dataService.cars);
-//
-// dt.appendToElement($('body'));
-
-
-// let tb = new TitleBar('Our Application');
-// tb.addLink('Home','javascript:;');
-// tb.addLink('Cars','javascript:;');
-// tb.addLink('Drones','javascript:;');
-// tb.addLink('Map','javascript:;');
-// tb.appendToElement($('body'));
-
-// let b = new Button('Click Me');
-// b.appendToElement($('body'));
-//
-// let i = new Image('../images/drone.jpg');
-// i.appendToElement($('body'));
-
-
-// let dataService = new FleetDataService();
-// dataService.loadData(fleet);
-
-// for (let car of dataService.cars)
-//   console.log(car.license);
-//
-// for (let drone of dataService.drones)
-//   console.log(drone.airTimeHours);
-//
-// for (let e of dataService.errors)
-//   console.log(e.message);
-//
-//
-// let car = dataService.getCarByLicense('AT9900');
-// console.log(car);
-//
-// let cars = dataService.getCarsSortedByLicense();
-// for(let car of cars)
-//   console.log(car.license);
-//
-// let filteredCars = dataService.filterCarsByMake('e');
-// for(let car of filteredCars)
-//   console.log(car.make);
